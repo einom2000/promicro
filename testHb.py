3f8e5c9a10ac2c011282105eb5582373a765f315
einom2000 <einom.ng@gmail.com>
2019-04-13 03:16:59 +0800

    111

--- testHb.py
+++ testHb.py
@@ -1,10 +1,20 @@
 import pyautogui
-import time
+import time,random
 import keyboard
 
 # while True:
 #     if keyboard.is_pressed(' '):
 #         print(pyautogui.position())
 #         time.sleep(3)
-left_pets =3
-print('now there are %d pets left.' % left_pets)
+# left_pets =3
+# print('now there are %d pets left.' % left_pets)
+
+while True:
+    if keyboard.is_pressed(' '):
+        break
+
+for i in range(3000):
+    pyautogui.press('f')
+    time.sleep(random.randint(50000, 80000) / 100000)
+    if keyboard.is_pressed(' '):
+        break
